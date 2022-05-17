dec654a54384f602dd1afa2dcea022dc641532a2
mackgabe <mackenzieabe@gmail.com>
2022-05-17 09:02:04 -0400

    added notes explaining the functions I put in place

--- script.js
+++ script.js
@@ -15,25 +15,25 @@ function generatePassword() {
     var password = ""
     var passwordChars = ""
 
-    // ask the user what kind of characters they want 
+    // ask the user how many characters they want in their password
     var pwLength = prompt("Between 8 and 128, how long would you like your password to be?")
     while (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
         alert("Your password must be a number and can only be between 8 and 128 characters long")
         pwLength = prompt("Between 8 and 128, how long would you like your password to be?")
     }
 
-// 
-    var wantsUpper = confirm("Would you like uppercase letters?")
-    var wantsLower = confirm("Would you like lowercase letters?")
-    var wantsNumbers = confirm("Would you like numbers?")
-    var wantsSpecialChar = confirm("Would you like special characters?")
-// 
+// ask the user what kind of characters they want to use in their password 
+    var wantsUpper = confirm("Would you like your password to have uppercase letters?")
+    var wantsLower = confirm("Would you like your password to have lowercase letters?")
+    var wantsNumbers = confirm("Would you like your password to have numbers?")
+    var wantsSpecialChar = confirm("Would you like your password to have special characters?")
+// confirms that the user is selecting characters that match with the answer they gave the previous prompt
     while (!wantsUpper && !wantsLower && !wantsNumbers && !wantsSpecialChar) {
         alert("You must choose at least one type of character")
-        wantsUpper = confirm("Would you like uppercase letters?")
-        wantsLower = confirm("Would you like lowercase letters?")
-        wantsNumbers = confirm("Would you like numbers?")
-        wantsSpecialChar = confirm("Would you like special characters?")
+        wantsUpper = confirm("Would you like your password to have uppercase letters?")
+        wantsLower = confirm("Would you like your password to have lowercase letters?")
+        wantsNumbers = confirm("Would you like your password to have numbers?")
+        wantsSpecialChar = confirm("Would you like your password to have special characters?")
     }
 // 
     if (wantsUpper) {
@@ -50,7 +50,7 @@ function generatePassword() {
     }
 
     console.log(passwordChars);
-// 
+// generate random characters for user password 
     for (var i = 0; i < pwLength; i++) {
         password += passwordChars.charAt(Math.floor(Math.random() * passwordChars.length))
     }
@@ -58,9 +58,8 @@ function generatePassword() {
     // return the password value
     return password
 }
-
 // Write password to the #password input
-//this is the event listener
+
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -69,5 +68,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button--this is the event listener
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
